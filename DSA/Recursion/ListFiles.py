0aac7920b8f2eb92ae0a99745b988103acf73ef8
scoutjohn <scoutjohn1@gmail.com>
2024-11-18 13:58:49 +0530

    List directories using recursion

---
+++ DSA/Recursion/ListFiles.py
@@ -0,0 +1,14 @@
+import os
+
+
+def print_file_in_subdirectories(path_name):
+    for filename in os.listdir(path_name):
+        path=os.path.join(path_name,filename)
+        if os.path.isdir(path):
+            print_file_in_subdirectories(path)
+        else:
+            print(path)
+
+
+if __name__ == '__main__':
+    print_file_in_subdirectories("/Users/scoutjohn13/Documents/Python/pycharm_projects/pythonProject/DSA")
